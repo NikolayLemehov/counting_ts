cf4fd6cc3df2bf88ef21eb789ea7428cd65a1232
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-31 15:14:23 +0300

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-// import OperationList from "./components/OperationList";
 import ListTable from "./components/ListTable";
 import {Container} from "@mui/material";
 import MyForm from "./components/MyForm/MyForm";
-// import OperationList from "./components/OperationList";
-// import {Paper, TextField} from "@mui/material";
-// import UserList from "./components/UserList";
 
 function App() {
   return (
     <Container>
+      <h1>Counting (TypeScript)</h1>
       <MyForm/>
-      {/*<TextField/>*/}
-      {/*<Paper />*/}
       <ListTable/>
-      {/*<OperationList/>*/}
     </Container>
   );
 }
