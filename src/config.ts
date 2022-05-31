c2d100980e6bdb5749abbd719a35c2563c51c141
NikolayLiemiekhov <nicklemster@gmail.com>
2022-05-31 19:49:23 +0300

    fix env variables

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 const LOCAL_URL: string | undefined = process.env.REACT_APP_SERVER
-
 export const URL_API: string = LOCAL_URL || 'https://counting-server-220515.herokuapp.com/'
+// console.log('LOCAL_URL', LOCAL_URL)
+// console.log('URL_API', URL_API)
 export const NBU_COURSE_API: string = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange'
